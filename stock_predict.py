988b05880cb12a29410a1cda6169cb6d7a9024fd
Le Vu <lmvu103@gmail.com>
2022-12-17 10:34:30 +0700

    Update stock_predict.py

--- stock_predict.py
+++ stock_predict.py
@@ -44,7 +44,7 @@ else:
 
     vnd = batdata.Vnd()
     # History data load
-    data = vnd.hist(selected_stock, "close", ""20100101", "20200101")
+    data = vnd.hist(selected_stock, "close", "20100101", "20200101")
     #jsonData = data.json  # getting data as json
     dfData = data.df  # getting data as pandas DataFrame
     dfData['tradingDate'] = pd.to_datetime(dfData['tradingDate'], format='%Y-%m-%d')  # format date date
