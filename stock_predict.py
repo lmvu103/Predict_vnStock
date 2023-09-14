8a738874d22269944680e422e54d765f8e172805
Le Vu <lmvu103@gmail.com>
2023-09-14 09:12:50 +0700

    Update stock_predict.py

--- stock_predict.py
+++ stock_predict.py
@@ -8,7 +8,7 @@ import streamlit as st
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
-import pystan
+#import pystan
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
 from keras.models import Sequential
