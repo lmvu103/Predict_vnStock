12d59b27bfc73c2ef5a29b87b74ba04c78efae9f
Le Vu <lmvu103@gmail.com>
2023-09-14 13:46:16 +0700

    Update stock_predict.py

--- stock_predict.py
+++ stock_predict.py
@@ -78,7 +78,7 @@ else:
     if (st.button('Press to predict Stock')):
         if method_predict == "Prophet":
             #st.write('Please choice LSTM')
-            #df_train = pd.DataFrame()
+            df_train = pd.DataFrame()
             #df_train['ds'] = df['date']
             df_train['y'] = df['close']
             m = Prophet()
