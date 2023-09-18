5f547dd7e8c67f2d1e3b8078c6fd0e143e6ec4cc
Le Vu <lmvu103@gmail.com>
2023-09-18 21:34:35 +0700

    Update stock_predict.py

--- stock_predict.py
+++ stock_predict.py
@@ -135,8 +135,8 @@ else:
             test_data = scaled_data[training_data_len - period: , :]
             x_test = []
             for i in range (period, len(test_data)):
-                x_test.append(test_data[i -period:i, 0])
-                x_test = np.array(x_test)
+                x_test.append(test_data[i-period:i, 0])
+            x_test = np.array(x_test)
             x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
             predictions = regressor.predict(x_test)
             predictions = scaler.inverse_transform(predictions)
